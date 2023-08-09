486d9a5f41f83f49d5678191dce71b347d8ec73d
Dmitry Kumachev <dkumachev18@gmail.com>
2023-08-09 22:52:15 +0400

    несколько мнгновений поработал со стилями

--- src/App.js
+++ src/App.js
@@ -6,15 +6,18 @@ import Home from './components/Content/Home/Home';
 import About from './components/Content/About/About';
 import Contacts from './components/Content/Contacts/Contacts';
 import GitHub from './components/Content/GitHub/GitHub';
-import ContentWrapper from './components/Content/ContentWrapper';
 import Weather from './components/Content/Weather/Weather';
 import ArtGenerator from './components/Content/ArtGenerator/ArtGenerator';
 import Countries from './components/Content/Countries/Countries';
+import Header from './components/Header/Header';
+import Cursor from './components/Cursor/Cursor';
 
 function App() {
   return (
     <BrowserRouter>
-      <ContentWrapper />
+    <div className="main-container">
+      <Header />
+      <Cursor />
       <Routes>
         <Route path='/' element={<MainPage />} />
         <Route path='/home' element={<Home />} />
@@ -25,6 +28,10 @@ function App() {
         <Route path='/art' element={<ArtGenerator />} />
         <Route path='/countries' element={<Countries />} />
       </Routes>
+    </div>
+    <style>
+    @import url('https://fonts.googleapis.com/css2?family=Raleway:wght@100;200;300;400;600;900&family=Ubuntu:wght@300;400;500;700&display=swap');
+    </style>
     </BrowserRouter>
   );
 }
