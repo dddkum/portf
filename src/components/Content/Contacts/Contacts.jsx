ab085136a1eef007b94a4adccae82b02870d1a2b
Dmitry Kumachev <dkumachev18@gmail.com>
2023-08-16 14:49:45 +0400

    Удалил компонент Github

--- src/components/Content/Contacts/Contacts.jsx
+++ src/components/Content/Contacts/Contacts.jsx
@@ -6,9 +6,10 @@ const Contacts = () => {
   return (
     <div>
     <h1>Мои контакты:</h1>
+    <h3>Номер телефона: 89827976137</h3>
     <h3>Telegram : @dddddkum</h3>
     <h3><Link to='https://vk.com/dddkum'>VK</Link></h3>
-    <h3>Номер телефона: 89827976137</h3>
+    <h3><Link to='https://github.com/dddkum'>Профиль Github</Link></h3>
   </div>
   )
 }
